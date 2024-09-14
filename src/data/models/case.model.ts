15da685cf4b47d777be68c75d7740e4994d24098
Emilio <guimelemilio@gmail.com>
2024-09-14 13:26:43 -0600

    Ajuste de schema de mongoose

--- src/data/models/case.model.ts
+++ src/data/models/case.model.ts
@@ -20,12 +20,12 @@ const caseSchema = new mongoose.Schema({
     },
     age: {
         type: Number,
-        required: false,
-        default: 0
+        required: true,
+         default: 0
     },
     creationDate: {
         type: Date,
-        required: false,
+        required: true,
         
     }
 });
